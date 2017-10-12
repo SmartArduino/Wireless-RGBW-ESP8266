ff2fd968c481353e58919822d834726ed6ca0e25
J6B <J6B@hotmail.fr>
2017-10-12 12:51:14 +0200

    Switch to CDN Jquery first

--- htmlToCppHeader/Convert file to code.ps1
+++ htmlToCppHeader/Convert file to code.ps1
@@ -50,7 +50,7 @@ Compress-7Zip -Path $path -Filter $file -ArchiveFileName ($path+"\"+$file+".gz")
 BinaryToCppHeader($path+"\"+$file+".gz")
 Remove-Item ($path+"\"+$file+".gz")
 
-$file="jquery-3.1.1.min.js"
+$file="jquery-3.2.1.min.js"
 Compress-7Zip -Path $path -Filter $file -ArchiveFileName ($path+"\"+$file+".gz") -CompressionLevel Ultra
 BinaryToCppHeader($path+"\"+$file+".gz")
 Remove-Item ($path+"\"+$file+".gz")
